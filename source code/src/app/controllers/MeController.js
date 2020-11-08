ec562d1867250080f6701a50023038d85455cc6c
whitehat09 <hoangngocthangofficer@gmail.com>
2020-11-08 10:59:02 +0700

    delete counts documents

--- source code/src/app/controllers/MeController.js
+++ source code/src/app/controllers/MeController.js
@@ -6,9 +6,11 @@ const mongoose = require('../../util/mongoose');
 class MeController {
     // [get]  /me/stored/courses
     storedCourses(req, res, next) {
-        Course.find({})
-            .then((courses) =>
+        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+            // xử lý bất đồng bộ giưa tìm và đếm số lượng bị xoá
+            .then(([courses, deleteCount]) =>
                 res.render('me/stored-courses', {
+                    deleteCount,
                     courses: mutipleMongooseToObject(courses),
                 }),
             )
@@ -17,7 +19,7 @@ class MeController {
 
     // [get]  /me/trash/courses
     trashCourses(req, res, next) {
-        Course.findDeleted({})
+        Course.findDeleted({}) // tìm đã xoá
             .then((courses) =>
                 res.render('me/trash-courses', {
                     courses: mutipleMongooseToObject(courses),
